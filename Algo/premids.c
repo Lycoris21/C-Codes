570f4df9992325cb0b4c7cb14ccec4b4096dc72a
Kwesten <christineanndejito21@gmail.com>
2025-03-06 11:44:32 +0800

    Premids for Algo iz prog2 lezgooo

---
+++ Algo/premids.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define LENGTH 100
+
+typedef struct{
+   int elem[LENGTH];
+   int count;
+}List;
+
+List TwoWaySortMerge(List A, List B);
+
+int main() {
+    List A = {{1,8,7}, 3};
+    List B = {{2, 3, 9, 11}, 4};
+    List C = TwoWaySortMerge(A, B);
+    for(int x=0; x<C.count; x++){
+        printf("%d ", C.elem[x]);
+    }
+    return 0;
+}
+
+List TwoWaySortMerge(List A, List B){
+    List C;
+    C.count = 0;
+    int x, y, z;
+    x = y = z = 0;
+    while(x<A.count && y<B.count){
+        C.elem[z++] = (A.elem[x] < B.elem[y])? (A.elem[x++]) : (B.elem[y++]);
+        C.count++;
+    }
+    if(x==A.count){
+        A = B;
+    }
+    while(y<A.count){
+        C.elem[z++] = A.elem[y++];
+        C.count++;
+    }
+    return C;
+}
